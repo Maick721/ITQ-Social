889831a9b08e4977cc91e847b722a44547e42b1c
Maick721 <maiccolfrancisco@gmail.com>
2025-11-11 18:14:55 -0500

    muro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
 
 export const routes: Routes = [
     {path: "home", component: HomeComponent},
+    {path: "navbar", component: NavbarComponent}
 ];
--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
+import { MuroComponent } from '../muro/muro.component'; 
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NavbarComponent, MuroComponent ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
