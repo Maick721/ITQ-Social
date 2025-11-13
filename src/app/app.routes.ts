a5bba76696eebd30faa46e9b7d12fa2687e9886b
Maick721 <maiccolfrancisco@gmail.com>
2025-11-12 23:11:36 -0500

    Creacion del Logiin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
+import { LoginComponent } from './component/login/login.component';
 
 export const routes: Routes = [
     {path: "home", component: HomeComponent},
-    {path: "navbar", component: NavbarComponent}
+    {path: "navbar", component: NavbarComponent},
+    {path: "login", component: LoginComponent}
 ];
